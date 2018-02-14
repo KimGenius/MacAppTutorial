7ae320b5bb9634c82b54ebe087a66653e079a963
KimGenius <geniusk1047@naver.com>
2018-02-14 12:56:39 +0900

    appcode 연동 및 .gitignore 뒤늦게 추가

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var nameField: NSTextField!
     @IBOutlet weak var helloLabel: NSTextField!
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,25 +22,25 @@ class ViewController: NSViewController {
 
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
 
     @IBAction func sayButtonClicked(_ sender: Any) {
         var name = nameField.stringValue
         var result = "Hello"
-        print("value : "+name)
+        print("value : " + name)
         if name.isEmpty {
             name = "World"
         }
         result += name
-        print("result : "+result, terminator:" \n")
+        print("result : " + result, terminator: " \n")
         // terminator:"" 속성은 "" 안에 들어간 내용으로 마지막에 넣는다.
         // ex) terminator:"" -> 아무 값도 안함, 즉 기본인 개행도 하지 않음
         // ex) terminator:" " -> 마지막마다 한칸 띄어쓰기 함
         // 개행문자는 다른거와 같이 \n 인듯.
         helloLabel.stringValue = result
     }
-    
+
 }
 
