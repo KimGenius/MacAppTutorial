b95ec1f9ead73e81ac492e6ca28f1812f495c757
KimGenius <geniusk1047@naver.com>
2018-02-14 12:26:49 +0900

    Initial Commit

---
+++ HelloWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by 김영재 on 2018. 2. 14..
+//  Copyright © 2018년 김영재. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
